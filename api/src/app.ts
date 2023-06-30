d8d1cd7e6c20265a6fb74244b699da8fe74499a7
Tomas <tomascerdeyraimport@gmail.com>
2023-06-30 02:27:09 -0300

    User Model

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import cors from "cors";
+import cors from 'cors'
 import "dotenv/config";
+import userRoute from './routes/user.routes'
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/', userRoute)
 
 const PORT = process.env.PORT || 8080;
 
---
+++ api/src/interface/user.interface.ts
@@ -0,0 +1,8 @@
+interface user {
+    name: string,
+    email: string,
+    messages: [],
+    password: string
+}
+
+export default user
