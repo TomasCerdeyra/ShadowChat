c5cbce8b80c78acd4c0e94501445cf77f4f1980f
Lucio <luciogastellu.dev@gmail.com>
2023-06-30 02:23:50 -0300

    Mongo DB Connection ready

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,19 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectMongodb from "./config/connectMongo.config";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+connectMongodb();
+
+app.get('*', (req: Request, res: Response) => {
+    res.status(404).json({ message: 'Route not found' });
+})
 
 const PORT = process.env.PORT || 8080;
 
